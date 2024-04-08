61c1ebb2b52a9662a829b9b928abb8bf8745737b
abhay <abhaytxanax@gmail.com>
2024-04-08 23:31:09 +0530

    app-config

---
+++ admin_panel_backend_heavy/server/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import bodyParser, { urlencoded } from "body-parser";
+import { Mongoose } from "mongoose";
+import cors from "cors";
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from "morgan";
+
+/* CONFIGURATIONS */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
