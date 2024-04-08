c1e2843f849b790e32f3c535bc30e9df2d1700d8
abhay <abhaytxanax@gmail.com>
2024-04-09 05:01:51 +0530

    theme and store provider

--- admin_panel_backend_heavy/client/src/App.js
+++ admin_panel_backend_heavy/client/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
-      hello
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
     </div>
   );
 }
