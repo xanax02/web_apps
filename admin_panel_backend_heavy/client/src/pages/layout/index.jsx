83ef9152834867ad6787b2bb76bf81dc1abd2ebf
abhay <abhaytxanax@gmail.com>
2024-04-09 20:30:52 +0530

    flexBetween styled component

--- admin_panel_backend_heavy/client/src/pages/layout/index.jsx
+++ admin_panel_backend_heavy/client/src/pages/layout/index.jsx
@@ -1,1 +1,9 @@
-export default function Layout() {}
+import { useState } from "react";
+import { Box, useMediaQuery } from "@mui/material";
+import { Outlet } from "@mui/icons-material";
+import { useSelector } from "react-redux";
+import Navbar from "components/Navbar";
+
+export default function Layout() {
+  return <Box width="100%" height="100%"></Box>;
+}
