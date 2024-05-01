bd6cdcfb5c97ced8d22817d1705f5f632fc15ea8
abhay <abhaytxanax@gmail.com>
2024-05-01 21:49:06 +0530

    some changes

--- admin_panel_backend_heavy/client/src/pages/layout/index.jsx
+++ admin_panel_backend_heavy/client/src/pages/layout/index.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
 import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
-import { useSelector } from "react-redux";
 import Navbar from "components/Navbar";
 import Sidebar from "components/Sidebar";
 
 export default function Layout() {
-  const isNonMobile = useMediaQuery("(min-width: 600px");
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
   return (
