bb311aaccff657c4ee97280737869d280cabd9fa
abhay <abhaytxanax@gmail.com>
2024-06-09 02:01:28 +0530

    toolkit query for userdata

--- admin_panel_backend_heavy/client/src/store/index.js
+++ admin_panel_backend_heavy/client/src/store/index.js
@@ -1,12 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modeReducer from "store/modeSlice";
 import authReducer from "store/authSlice";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { api } from "./query/api";
 
 const store = configureStore({
   reducer: {
     mode: modeReducer,
     auth: authReducer,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefault) => getDefault().concat(api.middleware),
 });
+setupListeners(store.dispatch);
 
 export default store;
