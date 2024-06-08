c039a6f397f8434276c178700759b9256ffd98f9
abhay <abhaytxanax@gmail.com>
2024-06-09 02:21:56 +0530

    user detials

--- admin_panel_backend_heavy/client/src/components/Sidebar.jsx
+++ admin_panel_backend_heavy/client/src/components/Sidebar.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import {
   Box,
-  Devider,
   Drawer,
   IconButton,
   List,
@@ -92,6 +91,7 @@ const navItems = [
 ];
 
 export default function Sidebar({
+  user,
   drawerWidth,
   isSidebarOpen,
   setIsSidebarOpen,
@@ -193,6 +193,35 @@ export default function Sidebar({
                 );
               })}
             </List>
+          </Box>
+          <Box position={"absolute"} bottom={"2rem"}>
+            {/* <Devider /> */}
+            <FlexBetween
+              textTransform={"none"}
+              gap={"1rem"}
+              m="1.5rem 2rem 0 3rem"
+            >
+              <Box
+                component={"img"}
+                alt="profile"
+                src={ProfileImage}
+                height={"40px"}
+                width={"40px"}
+                borderRadius={"50%"}
+                sx={{ objectFit: "cover" }}
+              />
+              <Box textAlign={"left"}>
+                <Typography
+                  fontSize={"0.8rem"}
+                  sx={{ color: theme.palette.secondary[200] }}
+                >
+                  {user.occupation}
+                </Typography>
+              </Box>
+              <SettingsOutlined
+                sx={{ color: theme.palette.secondary[300], fontSize: "25px" }}
+              />
+            </FlexBetween>
           </Box>
         </Drawer>
       )}
--- admin_panel_backend_heavy/client/src/pages/layout/index.jsx
+++ admin_panel_backend_heavy/client/src/pages/layout/index.jsx
@@ -3,14 +3,20 @@ import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import Navbar from "components/Navbar";
 import Sidebar from "components/Sidebar";
+import { useGetUserQuery } from "../../store/query/api";
+import { useSelector } from "react-redux";
 
 export default function Layout() {
   const isNonMobile = useMediaQuery("(min-width: 600px)");
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const userId = useSelector((state) => state.auth.userId);
+  const { data } = useGetUserQuery(userId);
+  console.log(data);
 
   return (
     <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
       <Sidebar
+        user={data || {}}
         isNonMobile={isNonMobile}
         drawerWidth="250px"
         isSidebarOpen={isSidebarOpen}
@@ -18,6 +24,7 @@ export default function Layout() {
       />
       <Box>
         <Navbar
+          user={data || {}}
           isSidebarOpen={isSidebarOpen}
           setIsSidebarOpen={setIsSidebarOpen}
         />
