426af77e4edb45f33ff3142529bbba39178da98c
abhay <abhaytxanax@gmail.com>
2024-06-19 22:54:30 +0530

    server error debugged

--- admin_panel_backend_heavy/client/src/store/query/api.js
+++ admin_panel_backend_heavy/client/src/store/query/api.js
@@ -4,10 +4,12 @@ export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
   reducerPath: "adminApi",
   tagTypes: ["User"],
-  endpoints: (builder) => {
+  endpoints: (builder) => ({
     getUser: builder.query({
-      query: (id) => `general/user/${id}`,
+      query: (id) => `/general/user/${id}`,
       providesTags: ["User"],
-    });
-  },
+    }),
+  }),
 });
+
+export const { useGetUserQuery } = api;
--- admin_panel_backend_heavy/server/controllers/general.js
+++ admin_panel_backend_heavy/server/controllers/general.js
@@ -1,9 +1,9 @@
 import User from "../models/User.js";
 
-export const getUser = (req, res) => {
+export const getUser = async (req, res) => {
   try {
     const { id } = req.params;
-    const user = User.findById(id);
+    const user = await User.findById(id);
     res.status(200).json(user);
   } catch (err) {
     res.status(402).json({ message: err.message });
