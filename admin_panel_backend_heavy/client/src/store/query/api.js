561c063fb3278c430fdc5d40689829104c0384f8
abhay <abhaytxanax@gmail.com>
2024-06-19 15:14:18 +0530

    apiSlice

--- admin_panel_backend_heavy/client/src/store/query/api.js
+++ admin_panel_backend_heavy/client/src/store/query/api.js
@@ -4,12 +4,10 @@ export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
   reducerPath: "adminApi",
   tagTypes: ["User"],
-  endpoints: (build) => ({
-    getUser: build.query({
-      query: (id) => `/general/user/${id}`,
+  endpoints: (builder) => {
+    getUser: builder.query({
+      query: (id) => `general/user/${id}`,
       providesTags: ["User"],
-    }),
-  }),
+    });
+  },
 });
-
-export const { useGetUserQuery } = api;
