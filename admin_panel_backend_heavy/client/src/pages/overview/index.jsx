791bdd3a76519fa9986255027a52e9d6ba25d532
abhay <abhaytxanax@gmail.com>
2024-06-25 22:21:15 +0530

    monthly sales/units overview

--- admin_panel_backend_heavy/client/src/pages/overview/index.jsx
+++ admin_panel_backend_heavy/client/src/pages/overview/index.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { useGetSalesQuery } from "store/query/api";
 import { Box, FormControl, MenuItem, InputLabel, Select } from "@mui/material";
 import Header from "components/Header";
 import OverviewChat from "components/OverviewChat";
