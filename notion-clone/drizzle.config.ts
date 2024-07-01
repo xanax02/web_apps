05fc51a0dc1b7602b8992a8d86f2ad56fdff8f8f
abhay <abhaytxanax@gmail.com>
2024-07-01 21:59:25 +0530

    drizzle config

---
+++ notion-clone/drizzle.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "drizzle-kit";
+import * as dotenv from "dotenv";
+dotenv.config({ path: ".env" });
+
+if (!process.env.DATABASE_URL) {
+  console.log("CANNOT FIND DB URL");
+}
+
+export default {
+  schema: "./src/lib/supabase/schema.ts",
+};
