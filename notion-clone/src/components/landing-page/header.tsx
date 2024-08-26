d69dc10419e2bdb7d48822d1dbbcb6d0bbf5f0bb
abhay <abhaytxanax@gmail.com>
2024-08-27 00:43:35 +0530

    header component init

---
+++ notion-clone/src/components/landing-page/header.tsx
@@ -0,0 +1,98 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import Logo from "../../../public/cypresslogo.svg";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { cn } from "@/lib/utils";
+import { Button } from "../ui/button";
+
+const routes = [
+  { title: "Features", href: "#features" },
+  { title: "Reasources", href: "#resources" },
+  { title: "Pricing", href: "#pricing" },
+  { title: "Testimonials", href: "#testimonial" },
+];
+
+const components: { title: string; href: string; description: string }[] = [
+  {
+    title: "Alert Dialog",
+    href: "#",
+    description:
+      "A modal dialog that interrupts the user with important content and expects a response.",
+  },
+  {
+    title: "Hover Card",
+    href: "#",
+    description:
+      "For sighted users to preview content available behind a link.",
+  },
+  {
+    title: "Progress",
+    href: "#",
+    description:
+      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+  },
+  {
+    title: "Scroll-area",
+    href: "#",
+    description: "Visually or semantically separates content.",
+  },
+  {
+    title: "Tabs",
+    href: "#",
+    description:
+      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+  },
+  {
+    title: "Tooltip",
+    href: "#",
+    description:
+      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+  },
+];
+
+const Header = () => {
+  const [path, setPath] = useState("#products");
+
+  return (
+    <header className="p-4 flex items-center justify-center">
+      <Link
+        href={"/"}
+        className="w-full flex gap-2
+        justify-left items-center"
+      >
+        <Image src={Logo} alt="Cypress Logo" width={25} height={25} />
+        <span
+          className="font-semibold
+          dark:text-white
+        "
+        >
+          cypress.
+        </span>
+      </Link>
+
+      {/* Navigation Items */}
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger></NavigationMenuTrigger>
+            <NavigationMenuContent></NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </header>
+  );
+};
+
+export default Header;
