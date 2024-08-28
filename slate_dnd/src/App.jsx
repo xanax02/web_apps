76d4465f09e7c90c66c534558b6aa96d3492d10c
abhay <abhaytxanax@gmail.com>
2024-08-28 18:32:16 +0530

    sensors

--- slate_dnd/src/App.jsx
+++ slate_dnd/src/App.jsx
@@ -1,12 +1,26 @@
 import "./App.css";
 
-import { DndContext } from "@dnd-kit/core";
+import {
+  DndContext,
+  KeyboardSensor,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
 import Draggable from "./components/draggable/Draggable";
 import Droppable from "./components/droppable/Droppable";
 
 function App() {
+  //multiple sesors
+  const mouseSensor = useSensor(MouseSensor);
+  const keyboardSensor = useSensor(KeyboardSensor);
+  const touchSensor = useSensor(TouchSensor);
+
+  const sensors = useSensors(mouseSensor, keyboardSensor, touchSensor);
+
   return (
-    <DndContext>
+    <DndContext sensors={sensors}>
       <Draggable />
       <Droppable />
     </DndContext>
