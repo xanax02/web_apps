f8ca9ae66f389f84ea9dfd59a17c32d22b74d446
abhay <abhaytxanax@gmail.com>
2024-09-04 02:19:57 +0530

    dashboard page

--- notion-clone/src/app/(main)/dashboard/page.tsx
+++ notion-clone/src/app/(main)/dashboard/page.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import db from "@/lib/supabase/db";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
-const page = () => {
-  return <div>Page</div>;
+const DashboardPage = async () => {
+  const supabase = createServerComponentClient({ cookies });
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user) return;
+
+  const workspace = await db.query.workspaces.findFirst({
+    where: (workspace, { eq }) => eq(workspace.workspaceOwner, user.id),
+  });
+
+  if (!workspace) return <h2>New workspace will be created</h2>;
+
+  redirect(`/dashboard/${workspace.id}`);
 };
 
-export default page;
+export default DashboardPage;
