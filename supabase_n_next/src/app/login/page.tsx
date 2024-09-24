ae43960b888e29172b3472c5cccc42336f41250f
abhay <abhaytxanax@gmail.com>
2024-09-24 22:59:39 +0530

    middleware

---
+++ supabase_n_next/src/app/login/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+    </div>
+  );
+};
+
+export default LoginPage;
--- supabase_n_next/src/app/page.tsx
+++ supabase_n_next/src/app/page.tsx
@@ -1,16 +1,5 @@
 import { supabase } from "@/lib/supabase";
 
 export default function Home() {
-  const setNewView = async () => {
-    const { data, error } = await supabase
-      .from("views")
-      .insert({ name: "random name" });
-
-    if (data) console.log("data", data);
-    if (error) console.log("error", error);
-  };
-
-  setNewView();
-
-  return <div>Hello</div>;
+  return <div>LOGGED IN</div>;
 }
