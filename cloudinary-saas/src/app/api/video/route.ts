718eb29c264811ea1337c1045228950edb206f29
abhay <abhaytxanax@gmail.com>
2024-12-30 02:44:52 +0530

    prisma

---
+++ cloudinary-saas/src/app/api/video/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET(request: NextRequest) {
+  try {
+    const videos = await prisma.video.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return NextResponse.json(videos);
+  } catch (error) {
+    return NextResponse.json({ error: "Something went wrong", status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
