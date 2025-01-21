13e1d3cc3c15b36311d3ef34e374765c9d3ae57e
abhay <abhaytxanax@gmail.com>
2025-01-22 03:24:46 +0530

    signup-signin-routes-added

--- excalidraw/apps/http-backend/src/index.ts
+++ excalidraw/apps/http-backend/src/index.ts
@@ -5,12 +5,72 @@ import {
   SigninSchema,
 } from "@repo/common/types";
 import { prismaClient } from "@repo/db/client";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
 
 const app = express();
+app.use(express.json());
 
-app.post("/signup", (req, res) => {});
+app.post("/signup", async (req, res) => {
+  const parsedData = CreateUserSchema.safeParse(req.body);
 
-app.post("/signin", (req, res) => {});
+  if (!parsedData.success) {
+    res.status(403).json({ message: "invalid creds" });
+    return;
+  }
+
+  try {
+    // hash the pass :: TODO
+    const user = await prismaClient.user.create({
+      data: {
+        email: parsedData?.data?.username,
+        password: parsedData?.data?.password,
+        name: parsedData?.data?.name,
+      },
+    });
+
+    if (user) {
+      res.status(201).json({ user });
+      return;
+    }
+  } catch (e) {
+    res.status(411).json({
+      message: "User already exists",
+    });
+  }
+
+  res.status(500).json({ message: "internal server error" });
+  return;
+});
+
+app.post("/signin", async (req, res) => {
+  const parsedData = SigninSchema.safeParse(req.body);
+
+  if (!parsedData.success) {
+    res.status(403).json({ message: "invalid creds" });
+    return;
+  }
+
+  try {
+    const user = await prismaClient.user.findFirst({
+      where: {
+        email: parsedData?.data?.username,
+        password: parsedData?.data?.password,
+      },
+    });
+
+    if (!user) {
+      res.json(403).json({ message: "Incorrect email or password" });
+      return;
+    }
+    const token = jwt.sign({ userId: user.id }, JWT_SECRET);
+    res.status(200).json({ token });
+    return;
+  } catch (e) {
+    res.json(403).json({ message: e });
+    return;
+  }
+});
 
 app.get("/room", (req, res) => {});
 
