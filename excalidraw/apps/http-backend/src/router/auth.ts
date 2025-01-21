d97e7b46ca9a4c132a62ff44c6bb0c4255a0ed53
abhay <abhaytxanax@gmail.com>
2025-01-21 21:17:56 +0530

    excali-dropped

--- excalidraw/apps/http-backend/src/router/auth.ts
+++
@@ -1,8 +0,0 @@
-import express, { Router } from "express";
-import { signinController, signupController } from "../controller/auth";
-const router: Router = express.Router();
-
-router.post("/signup", signupController);
-router.post("/signin", signinController);
-
-export default router;
