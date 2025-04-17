98b1657bc03d89344eee298ff582ec5b2af38a76
abhay <abhaytxanax@gmail.com>
2025-04-18 05:11:27 +0530

    created folder component

--- loom-clone/src/components/global/folders/folder.tsx
+++ loom-clone/src/components/global/folders/folder.tsx
@@ -2,6 +2,8 @@
 import { usePathname, useRouter } from "next/navigation";
 import Loader from "../loader";
 import { cn } from "@/lib/utils";
+import FolderDuotone from "@/components/icons/folder-duotone";
+import { useRef, useState } from "react";
 
 type Props = {
   name: string;
@@ -14,17 +16,40 @@ const Folder = ({ name, id, optimistic, count }: Props) => {
   const router = useRouter();
   const pathname = usePathname();
 
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const folderCardRef = useRef<HTMLDivElement | null>(null);
+  const [onRename, setOnRename] = useState(false);
+
+  const Rename = () => setOnRename(true);
+  const Renamed = () => setOnRename(false);
+
+  const handleFolderClick = () => {
+    router.push(`${pathname}/folder/${id}`);
+  };
+
+  const handleNameDoubleClick = (e: React.MouseEvent<HTMLParagraphElement>) => {
+    e.stopPropagation();
+  };
+
   return (
     <div
+      onClick={handleFolderClick}
       className={cn(
-        "flex hover:bg-neutral-800 cursor-pointer transition duration-150 items-center gap-2 justify-between min-w-[250px] py-6 px-4 rounded-lg border-[1px]"
+        "flex hover:bg-neutral-800 cursor-pointer transition duration-150 items-center gap-2 justify-between min-w-[250px] py-4 px-4 rounded-lg border-[1px] mt-4"
       )}
     >
       <Loader state={false}>
         <div className="flex flex-col gap-[1px]">
-            <p className="text-neutral-300">{name}</p>
+          <p
+            className="text-neutral-300"
+            onDoubleClick={(e) => handleNameDoubleClick(e)}
+          >
+            {name}
+          </p>
+          <span className="text-sm text-neutral-500">{count || 0} videos</span>
         </div>
       </Loader>
+      <FolderDuotone />
     </div>
   );
 };
--- loom-clone/src/components/global/folders/index.tsx
+++ loom-clone/src/components/global/folders/index.tsx
@@ -6,21 +6,25 @@ import Folder from "./folder"
 const Folders = ({workspaceId}: {workspaceId: any}) => {
     //getfolders
     //optimistic variable
-    return <div className="felx flex-col gap-4">
+    return (
+      <div className="felx flex-col gap-4">
         <div className="flex items-center justify-between">
-            <div className="flex items-center gap-4">
-                <FolderDuotone />
-                <h2 className="text-[#BDBDBD] text-xl">Folders</h2>
-            </div>
-            <div className="flex items-center gap-2">
-                <p className="text-[#BDBDBD]">See all</p>
-                <ArrowRight color="#707070" />
-            </div>
+          <div className="flex items-center gap-4">
+            <FolderDuotone />
+            <h2 className="text-[#BDBDBD] text-xl">Folders</h2>
+          </div>
+          <div className="flex items-center gap-2">
+            <p className="text-[#BDBDBD]">See all</p>
+            <ArrowRight color="#707070" />
+          </div>
         </div>
-        <section className={cn('flex items-center gap-4 overflow-x-auto w-full')} >
-            <Folder id="12445" name="Folder Title" />
+        <section
+          className={cn("flex items-center gap-4 overflow-x-auto w-full")}
+        >
+          <Folder id="12445" name="Folder Title" />
         </section>
-    </div>
+      </div>
+    );
 }
 
 export default Folders
