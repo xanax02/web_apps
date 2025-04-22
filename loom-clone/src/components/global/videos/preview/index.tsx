07a3f552309e26604332efbe7001d8e30cb504d4
abhay <abhaytxanax@gmail.com>
2025-04-23 01:42:19 +0530

    added video tag for preview page

--- loom-clone/src/components/global/videos/preview/index.tsx
+++ loom-clone/src/components/global/videos/preview/index.tsx
@@ -3,19 +3,64 @@
 import { useQueryData } from "@/hooks/useQueryData";
 import { getPreviewVideo } from "@/server-actions/workspace";
 import { videoProps } from "@/types/index.types";
+import { useRouter } from "next/navigation";
 
 type Props = {
   videoId: string;
 };
 
 const VideoPreview = ({ videoId }: Props) => {
+  const router = useRouter();
+
   const { data } = useQueryData(["preview-video"], () =>
     getPreviewVideo(videoId)
   );
 
   const { data: video, status, author } = data as videoProps;
 
-  return <div>VideoPreview</div>;
+  if (status != 200) router.push("/");
+
+  const daysAgo = Math.floor(
+    (new Date().getTime() - video.createdAt.getTime()) / (24 * 60 * 60 * 1000)
+  );
+
+  return (
+    <div className="grid grid-cols-1 xl:grid-cols-3 p-10 lg:px-20 lg:py-10 overflow-y-auto gap-5">
+      <div className="flex flex-col lg:col-span-2 gap-y-10">
+        <div>
+          <div className="flex gap-x-6 items-start justify-between">
+            <h2 className="text-white text-4xl font-bold">{video.title}</h2>
+            {/* {author ? (
+              <EditVideo
+                videoId={videoId}
+                title={video.title as string}
+                description={video.description as string}
+              />
+            ) : (
+              <></>
+            )} */}
+          </div>
+          <span className="flex gap-x-3 mt-2">
+            <p className="text-[#9D9D9D] capitalize">
+              {video.User?.firstname} {video.User?.lastname}
+            </p>
+            <p className="text-[#707070]">
+              {daysAgo === 0 ? "Today" : `${daysAgo}d ago`}
+            </p>
+          </span>
+        </div>
+        <video
+          preload="metadata"
+          className="w-full aspect-video opacity-50 rounded-xl"
+          controls
+        >
+          <source
+            src={`${process.env.NEXT_PUBLIC_CLOUD_FRONT_STREAM_URL}/${video.source}#1`}
+          />
+        </video>
+      </div>
+    </div>
+  );
 };
 
 export default VideoPreview;
