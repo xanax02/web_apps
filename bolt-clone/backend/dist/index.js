f642a9c311c7f56f91699df6fd384d81a6052df5
abhay <abhaytxanax@gmail.com>
2025-08-11 00:48:35 +0530

    added template route

--- bolt-clone/backend/dist/index.js
+++ bolt-clone/backend/dist/index.js
@@ -13,21 +13,63 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv").config();
+const express_1 = __importDefault(require("express"));
 const sdk_1 = __importDefault(require("@anthropic-ai/sdk"));
+const node_1 = require("./defaults/node");
+const react_1 = require("./defaults/react");
+const prompts_1 = require("./prompts");
 const anthropic = new sdk_1.default();
-function main() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const msg = yield anthropic.messages.stream({
-            // model: "claude-sonnet-4-20250514",
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.post("/template", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const prompt = req.body.prompt;
+        if (!prompt || prompt.trim().length === 0)
+            throw new Error("Prompt cant be empty");
+        const response = yield anthropic.messages.create({
+            messages: [{ role: "user", content: prompt }],
             model: "claude-3-haiku-20240307",
-            // model: "claude-3-5-sonnet-20241022",
             max_tokens: 200,
+            system: "Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra.",
             temperature: 0,
-            messages: [{ role: "user", content: "Create a very small landing page having only 3 section and very very little content" }],
         });
-        msg.on('text', (text) => {
-            console.log(text);
-        });
-    });
-}
-main();
+        const appTypeFromLLM = response.content[0].text;
+        if (appTypeFromLLM === "react") {
+            res.json({
+                prompts: [prompts_1.BASE_PROMPT, react_1.basePrompt],
+            });
+            return;
+        }
+        else if (appTypeFromLLM === "node") {
+            res.json({
+                prompts: [prompts_1.BASE_PROMPT, node_1.basePrompt],
+            });
+            return;
+        }
+        res.status(403).json({ message: "You cant access this" });
+        return;
+    }
+    catch (error) {
+        res.status(500).json({ error });
+    }
+}));
+app.listen(3000, () => console.log("server running at port 3000"));
+// async function main() {
+//     const msg = anthropic.messages.stream({
+//         // model: "claude-sonnet-4-20250514",
+//         model: "claude-3-haiku-20240307",
+//         // model: "claude-3-5-sonnet-20241022",
+//         max_tokens: 200,
+//         temperature: 0,
+//         messages: [{ role: "user", content: "Create a very small landing page having only 3 section and very very little content" }],
+//       });
+//     msg.on('text', (text) => {
+//       console.log(text);
+//     })
+// }
+// main();
+// TODO
+// system prompt
+// user prmopts
+// add templates for differnt apps
+// for next, vite, node
--- bolt-clone/backend/src/index.ts
+++ bolt-clone/backend/src/index.ts
@@ -2,51 +2,76 @@ require("dotenv").config();
 import express from "express";
 
 import Anthropic from "@anthropic-ai/sdk";
-import { MAX_TOKENS } from "./constants";
+// import { MAX_TOKENS } from "./constants";
 import { TextBlock } from "@anthropic-ai/sdk/resources/messages";
+import { basePrompt as nodeBasePrompt } from "./defaults/node";
+import { basePrompt as reactBasePrompt } from "./defaults/react";
+import { BASE_PROMPT } from "./prompts";
 
 const anthropic = new Anthropic();
 const app = express();
 app.use(express.json());
 
-app.post('/template', async(req, res) => {
-  const prompt = req.body.prompt;
+app.post("/template", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+    if (!prompt || prompt.trim().length === 0)
+      throw new Error("Prompt cant be empty");
 
-  const response = await anthropic.messages.create({
-    messages: [{role: "user", content: prompt}],
-    model: "claude-3-haiku-20240307",
-    max_tokens: MAX_TOKENS,
-    system: "Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra.",
-    temperature: 0
-  })
+    const response = await anthropic.messages.create({
+      messages: [{ role: "user", content: prompt }],
+      model: "claude-3-haiku-20240307",
+      max_tokens: 200,
+      system:
+        "Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra.",
+      temperature: 0,
+    });
 
-  const appTypeFromLLM = (response.content[0] as TextBlock).text;
-  if(appTypeFromLLM !== 'node' &&  appTypeFromLLM !== 'react')
-})
+    const appTypeFromLLM = (response.content[0] as TextBlock).text;
+
+    if (appTypeFromLLM === "react") {
+      res.json({
+        prompts: [BASE_PROMPT, reactBasePrompt],
+      });
+      return;
+    } else if (appTypeFromLLM === "node") {
+      res.json({
+        prompts: [BASE_PROMPT, nodeBasePrompt],
+      });
+      return;
+    }
+
+    res.status(403).json({ message: "You cant access this" });
+    return;
+  } catch (error) {
+    if (error instanceof Error) res.status(500).json({ error: error.message });
+    else res.status(500);
+    return;
+  }
+});
 
 app.listen(3000, () => console.log("server running at port 3000"));
 
-async function main() {
-    const msg = anthropic.messages.stream({
-        // model: "claude-sonnet-4-20250514",
-        model: "claude-3-haiku-20240307",
-        // model: "claude-3-5-sonnet-20241022",
-        max_tokens: 200,
-        temperature: 0,
-        messages: [{ role: "user", content: "Create a very small landing page having only 3 section and very very little content" }],
-      });
-    
-    msg.on('text', (text) => {
-      console.log(text);
-    })
-}
+// async function main() {
+//     const msg = anthropic.messages.stream({
+//         // model: "claude-sonnet-4-20250514",
+//         model: "claude-3-haiku-20240307",
+//         // model: "claude-3-5-sonnet-20241022",
+//         max_tokens: 200,
+//         temperature: 0,
+//         messages: [{ role: "user", content: "Create a very small landing page having only 3 section and very very little content" }],
+//       });
 
-main();
+//     msg.on('text', (text) => {
+//       console.log(text);
+//     })
+// }
 
+// main();
 
 // TODO
 // system prompt
 // user prmopts
 
 // add templates for differnt apps
-  // for next, vite, node
+// for next, vite, node
